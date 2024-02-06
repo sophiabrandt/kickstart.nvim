3bf02c5e40bf904b3d0d7b61c272ba7fc6f19a2b
Sophia Brandt <git@s-br.net>
2024-02-06 15:49:58 +0100

    update keybindings

--- lua/custom/plugins/keymaps.lua
+++ lua/custom/plugins/keymaps.lua
@@ -1,7 +1,9 @@
 return {
 	vim.keymap.set('n', 'U', '<C-r>', { noremap = true }), -- better Undo
 	vim.keymap.set('n', '<leader>=', vim.lsp.buf.format, { desc = 'Format code' }),
-	vim.keymap.set('n', '<leader>f', require('telescope.builtin').find_files, { desc = 'Search [F]iles' }),
+	vim.keymap.set('n', '<leader>f',
+		"<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>",
+		{ noremap = true, desc = 'Search [F]iles' }),
 	vim.api.nvim_set_keymap('n', '<leader>x', ':<C-u>bp<bar>bd! #<CR>',
 		{ silent = true, noremap = true, desc = "Close buffer" }),
 	vim.api.nvim_set_keymap('n', '<leader>X', ':<C-u>%bd<bar>e#<bar>bd#<CR>',
@@ -12,5 +14,5 @@ return {
 		[[<Cmd>lua vim.fn.system("tmux popup -h '80%' -w '80%' -E 'lazygit'")<CR>]],
 		{ noremap = true, silent = true }),
 	vim.api.nvim_set_keymap('i', 'hsr', '=>', { silent = true, noremap = true }),
-	vim.api.nvim_set_keymap('i', 'nra', '->', { silent = true, noremap = true }),
+	vim.api.nvim_set_keymap('i', 'hra', '->', { silent = true, noremap = true }),
 }
