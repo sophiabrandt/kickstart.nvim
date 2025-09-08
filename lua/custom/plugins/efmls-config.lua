be3dacafb257b3587f9cad9b03ca713547264d7b
Sophia Brandt <git@s-br.net>
2025-09-08 19:29:43 +0200

    fix typo, set fixed tag in efmls

--- lua/custom/plugins/efmls-config.lua
+++ lua/custom/plugins/efmls-config.lua
@@ -1,11 +1,11 @@
 return {
   'creativenull/efmls-configs-nvim',
-  version = 'v1.*.*', -- version is optional, but recommended
+  tag = 'v1.*',
   requires = { 'neovim/nvim-lspconfig' },
   config = function()
     local languages = require('efmls-configs.defaults').languages()
     languages = vim.tbl_extend('force', languages, {
-      -- you custom languages, or overrides
+      -- your custom languages, or overrides
       go = {
         require 'efmls-configs.linters.go_revive',
         require 'efmls-configs.formatters.gofumpt',
